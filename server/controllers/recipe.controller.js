1ca8e0480d6e7cf5b0ac82d88fe3d0f12936eeea
thanhtradev <thanhtra2001.stt@gmail.com>
2022-10-17 02:28:24 +0700

    add get top 10 recipe with most mlems

--- server/controllers/recipe.controller.js
+++ server/controllers/recipe.controller.js
@@ -119,3 +119,39 @@ exports.findOne = (req, res) => {
             res.send(recipe);
         });
 }
+
+exports.findTop10 = (req, res) => {
+    Recipe.find()
+        .sort({
+            number_of_mlems: -1
+        })
+        .limit(10)
+        .populate('user_id')
+        .populate('categories')
+        .exec((err, recipes) => {
+            if (err) {
+                res.status(500).send({
+                    message: err.message || "Some error occurred while retrieving recipes."
+                });
+                return;
+            }
+            recipeList = recipes.map(recipe => {
+                return {
+                    id: recipe._id,
+                    author: recipe.user_id.username,
+                    name: recipe.name,
+                    expert: recipe.expert,
+                    time: recipe.time,
+                    makes: recipe.makes,
+                    ingredients: recipe.ingredients,
+                    directions: recipe.directions,
+                    nutrition: recipe.nutrition,
+                    number_of_mlems: recipe.number_of_mlems,
+                    number_of_comments: recipe.number_of_comments,
+                    categories: recipe.categories.map(category => category.name),
+                    createdAt: recipe.createdAt,
+                }
+            });
+            res.status(200).send(recipeList);
+        });
+}
